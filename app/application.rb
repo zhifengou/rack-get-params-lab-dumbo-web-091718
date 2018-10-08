5aaf21c333e7f4c7b148bcf057456df8b261a325
zhifeng ou <zhifengou@hotmail.com>
2018-10-08 14:08:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,15 @@ class Application
 
   @@items = ["Apples","Carrots","Pears"]
   @@cart =[]
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack ::Request.new(env)
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end 
+    elsif 
 
   def call(env)
     resp = Rack::Response.new
